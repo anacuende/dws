6d8aa2e9ad96bb7b4da40ff409cbf6c816b437dc
Ana Betsabé Cuende Tomé <acuendet@fpcoruna.afundacion.org>
2022-09-29 10:55:48 +0200

    Ejercicio 4 completado (PHP)

---
+++ sprint1apache/www/site3/example4.php
@@ -0,0 +1,38 @@
+<html>
+	<body>
+		<h1>Jubilación</h1>
+
+		<?php
+		function edad_en_5_años($edad) {
+			return $edad + 5;
+		}
+
+
+		function mensaje($age) {
+			if (edad_en_5_años($age) > 65) {
+				return "En 5 años tendrás edad de jubilación";
+			} else {
+				return "¡Disfruta de tu tiempo!";
+			}
+		}
+		?>
+
+		<table>
+			<tr>
+				<th>Edad</th>
+				<th>Info</th>
+			</tr>
+
+		<?php
+		$lista = array(50,52,55,61,64);
+		foreach ($lista as $valor) {
+			echo "<tr>";
+			echo "<td>".$valor."</td>";
+			echo "<td>".mensaje($valor)."</td>";
+			echo "</tr>";
+		}
+		?>
+
+		</table>
+	</body>
+</html>
