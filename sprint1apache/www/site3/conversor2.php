d665521cc01c29d28a1db6a06297cc795e526cd9
Ana Betsabé Cuende Tomé <acuendet@fpcoruna.afundacion.org>
2022-09-29 11:55:15 +0200

    Ejercicio 6 completado (PHP)

--- sprint1apache/www/site3/conversor2.php
+++ sprint1apache/www/site3/conversor2.php
@@ -9,6 +9,10 @@
 				$v_pulgadas = $_POST["fcantidad"];
 				$v_metros = $v_pulgadas * 0.0254;
 				echo $v_pulgadas."pulgada(s) = ".$v_metros." metro(s)";
+			} elseif ($_POST["funidad"] == "yarda") {
+				$v_yardas = $_POST["fcantidad"];
+				$v_metros = $v_yardas * 1.09361;
+				echo $v_yardas."yarda(s) = ".$v_metros." metro(s)";
 			} else {
 				echo "Unidad no soportada";
 			}
@@ -22,6 +26,8 @@
 
 			<input type="radio" id="pulgada_input" name="funidad" value="pulgada">
 			<label for="pulgada_input">Pulgada(s)</label><br>
+			<input type="radio" id="yarda_input" name="funidad" value="yarda">
+			<label for="yarda_input">Yarda(s)</label><br>
 			<input type="radio" id="otro_input" name="funidad" value="otro">
 			<label for="otro_input">Otro</label><br>
 
