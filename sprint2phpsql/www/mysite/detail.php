a94f46dfd5d5fae0b7e46e1938781f29049d147a
ana <acuendet@fpcoruna.afundacion.org>
2022-10-13 12:53:05 +0200

    Ejercicio 4 terminado

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -12,10 +12,10 @@
 			$result = mysqli_query($db, $query) or die('Query error');
 			$only_row = mysqli_fetch_array($result);
 			echo '<h1>'.$only_row['id'].'</h1>';
-			echo '<h1>'.$only_row['nombre'].'</h1>';
+			echo '<h2>'.$only_row['nombre'].'</h2>';
 			echo '<h1><img src="'.$only_row['url_imagen'].'" width="100" height="100"></h1>';
-			echo '<h1>'.$only_row['autor'].'</h1>';
-			echo '<h1>'.$only_row['album'].'</h1>';
+			echo '<h2>'.$only_row['autor'].'</h2>';
+			echo '<h2>'.$only_row['album'].'</h2>';
 		?>
 		<h3>Comentarios:</h3>
 		<ul>
@@ -28,6 +28,11 @@
 					echo '<li>'.$row['usuario_id'].'</li>';
 					echo '<li>'.$row['cancion_id'].'</li>';
 				}
+					echo '<p>Añade un comentario</p>';
+					echo '<form action="/comment.php" method="post">';
+					echo '<textarea name="ncomentario" cols="40" rows="5" ></textarea><br>';
+					echo '<input type="hidden" name="cancion_id" value="'.$cancion_id.'">';
+					echo '<input type="submit" value="Comentar"></form>';
 				mysqli_close($db);
 			?>
 		</ul>
