860a99edcf807a7c7017423fca45d11cf2717065
ana <acuendet@fpcoruna.afundacion.org>
2022-10-13 10:46:09 +0200

    Ejercicio 2 completado

--- sprint2phpsql/www/mysite/main.php
+++ sprint2phpsql/www/mysite/main.php
@@ -1,4 +1,28 @@
 <?php
 	$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 ?>
-<h1>Conexión establecida</h1>
+<html>
+	<body>
+		<h1>Conexión establecida</h1>
+		<?php
+			// Lanzar una query
+			$query = 'SELECT * FROM tCanciones';
+			$result = mysqli_query($db, $query) or die('Query error');
+			// Recorrer el resultado
+			echo '<table border="1">';
+			echo '<tr><th>ID</th><th>Nombre</th><th>Foto</th><th>Autor</th><th>Álbum</th></tr>';
+			while ($row = mysqli_fetch_array($result)) {
+				echo '<tr>';
+				echo '<td><a href="/detail.php?id="'.$row['id'].'">id</a></td>';
+				echo '<td>'.$row['nombre'].'</td>';
+				echo '<td><img src="'.$row['url_imagen'].'" width="100" height="100"></td>';
+				echo '<td>'.$row['autor'].'</td>';
+				echo '<td>'.$row['album'].'</td>';
+				echo '</tr>';
+			}
+			echo '</table>';
+		mysqli_close($db);
+		?>
+	</body>
+</html>
+
