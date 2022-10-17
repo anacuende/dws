0ddf790a5e1187e7de9d7593e35e3588197f4c52
anacuende <113435331+anacuende@users.noreply.github.com>
2022-10-17 13:06:11 +0200

    Ejercicio 2 completado

--- sprint2python/concurso.py
+++ sprint2python/concurso.py
@@ -1,10 +1,37 @@
 r = '';
+puntos = 0;
+
 print('Cada cuántos años se celebran las Olimpiadas?');
 print('a) 2');
 print('b) 4');
 print('c) 10');
+
 r = input('Teclee a/b/c: ')
 if r == "b":
-    print('Correcto');
+    puntos = puntos + 10;
 else:
-    print('Incorrecto');
+    puntos = puntos - 5;
+
+print('Cuál de estos animales es un ave?');
+print('a) Perro');
+print('b) Delfín');
+print('c) Ágila');
+
+r = input('Teclee a/b/c: ')
+if r == "c":
+    puntos = puntos + 10;
+else:
+    puntos = puntos - 5;
+
+print('Cuál NO es un color primario?');
+print('a) Morado');
+print('b) Rojo');
+print('c) Azul');
+
+r = input('Teclee a/b/c: ')
+if r == "a":
+    puntos = puntos + 10;
+else:
+    puntos = puntos - 5;
+print("Puntos: ");
+print(puntos);
