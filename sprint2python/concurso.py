9fc9e802bcb3d67e60e56393d51b561e336a9037
anacuende <113435331+anacuende@users.noreply.github.com>
2022-10-17 12:56:01 +0200

    Add files via upload

---
+++ sprint2python/concurso.py
@@ -0,0 +1,10 @@
+r = '';
+print('Cada cuántos años se celebran las Olimpiadas?');
+print('a) 2');
+print('b) 4');
+print('c) 10');
+r = input('Teclee a/b/c: ')
+if r == "b":
+    print('Correcto');
+else:
+    print('Incorrecto');
