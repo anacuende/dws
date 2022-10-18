c815412f6b3574060a891fdad27c14c5e0d004ec
anacuende <113435331+anacuende@users.noreply.github.com>
2022-10-18 13:05:10 +0200

    Ejercicio 6 terminado

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,3 +1,4 @@
+import time
 from factorial import func_factorial;
 from factorial2 import func_factorial2;
 
@@ -9,7 +10,16 @@ print('a) func_factorial');
 print('b) func_factorial2');
 p = input('Opción a/b? ');
 n = int(input('Teclee número: '));
+
 if op == 'a':
-    print(func_factorial(n));
+    start_time = time.time()
+    print(func_factorial(n))
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
 else:
-    print(func_factorial2(n));
+    start_time = time.time()
+    print(func_factorial(n))
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
