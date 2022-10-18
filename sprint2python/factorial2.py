6ca196db925888892f48ff33202706a62897ddf3
anacuende <113435331+anacuende@users.noreply.github.com>
2022-10-18 12:49:28 +0200

    Ejercicio 4 completado

---
+++ sprint2python/factorial2.py
@@ -0,0 +1,12 @@
+def func_factorial2(n): 
+    if n == 0 or n == 1: 
+        return 1
+    else: 
+        factorial = 1
+        while(n > 1): 
+            factorial = factorial * n 
+            n = n - 1
+        return factorial 
+
+n = int(input('Teclee número: '));
+print(func_factorial2(n));
