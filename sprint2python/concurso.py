13997724e9a58b7773af0aed10d97e6d9615fde5
anacuende <113435331+anacuende@users.noreply.github.com>
2022-10-18 13:47:40 +0200

    Ejercicio 7 terminado

--- sprint2python/concurso.py
+++ sprint2python/concurso.py
@@ -1,37 +1,50 @@
+import random;
 r = '';
 puntos = 0;
+op = '';
 
-print('Cada cuántos años se celebran las Olimpiadas?');
-print('a) 2');
-print('b) 4');
-print('c) 10');
+pregunta = ['p1','p2','p3']
 
-r = input('Teclee a/b/c: ')
-if r == "b":
-    puntos = puntos + 10;
-else:
-    puntos = puntos - 5;
+def elegirPregunta(pregunta):
+    return random.choice(pregunta)
 
-print('Cuál de estos animales es un ave?');
-print('a) Perro');
-print('b) Delfín');
-print('c) Ágila');
+op = elegirPregunta(pregunta);
 
-r = input('Teclee a/b/c: ')
-if r == "c":
-    puntos = puntos + 10;
-else:
-    puntos = puntos - 5;
+if op == 'p1':
+    print('Cada cuántos años se celebran las Olimpiadas?');
+    print('a) 2');
+    print('b) 4');
+    print('c) 10');
 
-print('Cuál NO es un color primario?');
-print('a) Morado');
-print('b) Rojo');
-print('c) Azul');
+    r = input('Teclee a/b/c: ')
+    if r == "b":
+        puntos = puntos + 10;
+    else:
+        puntos = puntos - 5;
 
-r = input('Teclee a/b/c: ')
-if r == "a":
-    puntos = puntos + 10;
-else:
-    puntos = puntos - 5;
+elif op == 'p2':
+    print('Cuál de estos animales es un ave?');
+    print('a) Perro');
+    print('b) Delfín');
+    print('c) Ágila');
+
+    r = input('Teclee a/b/c: ')
+    if r == "c":
+        puntos = puntos + 10;
+    else:
+        puntos = puntos - 5;
+
+elif op == 'p3':
+    print('Cuál NO es un color primario?');
+    print('a) Morado');
+    print('b) Rojo');
+    print('c) Azul');
+
+    r = input('Teclee a/b/c: ')
+    if r == "a":
+        puntos = puntos + 10;
+    else:
+        puntos = puntos - 5;
+
 print("Puntos: ");
 print(puntos);
