9cfbd0acc44acb8d62470d525f113c8a9964bf38
ana <acuendet@fpcoruna.afundacion.org>
2022-10-19 14:24:52 +0200

    Ejercicio 1 completado

---
+++ sprint3sesiones/www/mysite/comment.php
@@ -0,0 +1,22 @@
+<?php
+	$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+ ?>
+<html>
+	<body>
+		<?php
+			$cancion_id = $_POST['cancion_id'];
+			$comentario = $_POST['ncomentario'];
+
+			$query = "INSERT INTO tComentarios(comentario, usuario_id, cancion_id, fecha) VALUES ('".$comentario."', NULL, ".$cancion_id.", current_date())";
+
+			mysqli_query($db, $query) or die('Error');
+
+			echo "<p>Nuevo comentario: ";
+			echo mysqli_insert_id($db);
+			echo " añadido</p>";
+
+			echo "<a href='/detail.php?id=".$cancion_id."'>Volver</a>";
+			mysqli_close($db);
+		?>
+	</body>
+</html>
